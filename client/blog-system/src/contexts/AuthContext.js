c620fb44c0be69b9feff68447897137816659c49
maria-tananska <maria.tananska@gmail.com>
2023-04-08 16:19:53 +0300

    create authContext

---
+++ client/blog-system/src/contexts/AuthContext.js
@@ -0,0 +1,52 @@
+import { createContext, useState, useContext } from 'react';
+import * as authService from '../services/authService';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const [loginIsOpen, setLoginIsOpen] = useState(false);
+    const [user, setUser] = useLocalStorage('user', {});
+
+    const openLoginDialog = () => {
+        setLoginIsOpen(true);
+    };
+
+    const closeLoginDialog = () => {
+        setLoginIsOpen(false);
+    };
+
+    const onLoginSubmit = async (data) => {
+        try {
+            const response = await authService.login(data);
+            setUser(response);
+            setLoginIsOpen(false);
+        } catch (err) {
+            console.log(err.message,"error");
+        }
+    };
+
+    const contextValues = {
+        loginIsOpen,
+        user,
+        openLoginDialog,
+        closeLoginDialog,
+        onLoginSubmit
+    };
+
+    return (
+        <>
+            <AuthContext.Provider value={contextValues}>
+                {children}
+            </AuthContext.Provider>
+        </>
+    );
+};
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+
+    return context;
+};
