79e2952c374ba7c2c738514376b282904629b46b
maria-tananska <maria.tananska@gmail.com>
2023-04-09 19:30:53 +0300

    blog item style

--- client/blog-system/src/components/BlogItem/BlogItem.js
+++ client/blog-system/src/components/BlogItem/BlogItem.js
@@ -1,25 +1,33 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import styles from './BlogItem.module.css';
 
 export function BlogItem({
     _id,
+    _createdOn,
     title,
     imageUrl,
     shortDescription,
 }) {
+    const date = new Date(_createdOn).toDateString();
+
     return (
         <Card sx={{ maxWidth: 800 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: "#734FE6" }}>MT</Avatar>
+                }
+                title={<h2>{title}</h2>}
+                subheader={date}
+            />
             <CardMedia component="img" height="400" image={imageUrl} alt="green iguana" />
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {title}
-                </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {shortDescription}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Link to={`/posts/${_id}`}><Button size="small">Read More</Button></Link>
+                <Link className={styles.button}  to={`/posts/${_id}`}>Read More</Link>
             </CardActions>
         </Card>
     );
